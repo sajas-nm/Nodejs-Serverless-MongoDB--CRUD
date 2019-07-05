f6f8425e71068a75b1e23aef8d39d0f57fadb6f9
sajas24 <sajasmw@gmail.com>
2019-07-05 14:23:22 +0530

    crud

---
+++ handler.js
@@ -0,0 +1,159 @@
+const db = require('monk')('mongodb://node_client:pass@cluster0-shard-00-00-jierq.mongodb.net:27017,cluster0-shard-00-01-jierq.mongodb.net:27017,cluster0-shard-00-02-jierq.mongodb.net:27017/my-test-db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority');
+db.then(() => {
+    console.log('Connected correctly to server')
+}).catch(err => {
+    console.log(err)
+})
+
+
+const customers = db.get('customers')
+
+const createCustomer = (event) => {
+
+    const data = JSON.parse(event.body);
+    let name = data.name
+    let email = data.email
+    return new Promise((resolve, reject) => {
+        customers.insert({
+            name: name,
+            email: email
+        }).then(res => {
+            resolve({
+                "statusCode": 200,
+                "body": JSON.stringify({
+                    message: "Succesfully Inserted"
+                })
+            })
+        }).catch(err => {
+            console.log(err)
+            resolve({
+                "statusCode": 2,
+                "body": JSON.stringify({
+                    message: "Succesfully Inserted"
+                })
+            })
+        })
+
+    })
+}
+
+const updateCustomer = (event) => {
+    const id = event.pathParameters.id
+    let data = JSON.parse(event.body);
+    let name = data.name
+    let email = data.email
+    return new Promise((resolve, reject) => {
+
+        customers.findOneAndUpdate({
+            _id: id
+        }, {
+            $set: {
+                name: name,
+                email: email
+            }
+        }).then(res => {
+            resolve({
+                "statusCode": 200,
+                "body": JSON.stringify({
+                    message: "Updated Successfully...!"
+                })
+            })
+        }).catch(err => {
+            console.log(err)
+            resolve({
+                "statusCode": 2,
+                "body": JSON.stringify({
+                    message: "Somthing went Wrong Try again!"
+                })
+            })
+        })
+
+    })
+}
+
+const deleteCustomer = (event) => {
+    const id = event.pathParameters.id
+    console.log(id)
+    return new Promise((resolve, reject) => {
+
+        customers.remove({
+            _id: id
+        }).then(res => {
+            console.log(res)
+            resolve({
+                "statusCode": 200,
+                "body": JSON.stringify({
+                    message: "Customer Deleted Succesfully"
+                })
+            })
+        }).catch(err => {
+            console.log(err)
+            reject({
+                "statusCode": 400,
+                "body": JSON.stringify({
+                    Errors: err
+                })
+            })
+        })
+
+    })
+}
+
+const FindAllCustomers = (event) => {
+    return new Promise((resolve, reject) => {
+        customers.find({}).then(res => {
+            console.log(res)
+            resolve({
+                "statusCode": 200,
+                "body": JSON.stringify({
+                    results: res
+                })
+            })
+        }).catch(err => {
+            console.log(err);
+            reject({
+                "statusCode": 400,
+                "body": JSON.stringify({
+                    Errors: err
+                })
+            })
+        })
+    })
+}
+
+const FindCustomer = (event) => {
+    const id = event.pathParameters.id
+    console.log(id)
+    return new Promise((resolve, reject) => {
+
+        customers.findOne({
+            _id: id
+        }).then(res => {
+            console.log(res)
+            resolve({
+                "statusCode": 200,
+                "body": JSON.stringify({
+                    results: res
+                })
+            })
+        }).catch(err => {
+            console.log(err)
+            reject({
+                "statusCode": 400,
+                "body": JSON.stringify({
+                    Errors: err
+                })
+            })
+        })
+
+    })
+}
+
+
+module.exports = {
+    createCustomer,
+    updateCustomer,
+    deleteCustomer,
+    FindAllCustomers,
+    FindCustomer,
+}
